275fa70d5579026ddebd3e9a180abd2fe064d957
Matthew Flavin <mflavin@hawk.iit.edu>
2019-11-13 14:18:18 -0600

    Pushing change to test service worker.

--- app/index.js
+++ app/index.js
@@ -13,19 +13,24 @@ document.getElementById('reload').addEventListener('click', function(){
 if ('serviceWorker' in navigator) {
 
   navigator.serviceWorker.register('./service-worker.js').then(reg => {
+    console.log('reg');
     reg.addEventListener('updatefound', () => {
       // A wild service worker has appeared in reg.installing!
+      console.log('updatefound');
       newWorker = reg.installing;
 
       newWorker.addEventListener('statechange', () => {
+        console.log('statechange');
         // Has network.state changed?
         switch (newWorker.state) {
           case 'installed':
             if (navigator.serviceWorker.controller) {
               // new update available
+              console.log('new update available');
               showUpdateBar();
             }
             // No update available
+            console.log('No update available');
             break;
         }
       });
