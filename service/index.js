cc1d146281d12a175bbd10a2f57cad16eeab432a
Matthew Flavin <mflavin@hawk.iit.edu>
2019-11-13 10:01:37 -0600

    Pushing change to test service worker.

--- service/index.js
+++ service/index.js
@@ -17,43 +17,43 @@ document.addEventListener("DOMContentLoaded", function(){
     // Give an indicator that service workers are supported.
     status.textContent = 'supported';
 
-    // navigator.serviceWorker.register('./service-worker.js');
+    navigator.serviceWorker.register('./service-worker.js');
 
-    navigator.serviceWorker.register('./service-worker.js').then((reg) => {
-      console.log(reg, 'reg');
-      // updatefound is fired if service-worker.js changes.
-      reg.onupdatefound = function () {
-        console.log('onupdatefound');
-        // The updatefound event implies that reg.installing is set; see
-        // https://w3c.github.io/ServiceWorker/#service-worker-registration-updatefound-event
-        const installingWorker = reg.installing;
-
-        installingWorker.onstatechange = function () {
-          console.log('onstatechange');
-          switch (installingWorker.state) {
-            case 'installed':
-              if (navigator.serviceWorker.controller) {
-                // At this point, the old content will have been purged and the fresh content will
-                // have been added to the cache.
-                // It's the perfect time to display a "New content is available; please refresh."
-                // message in the page's interface.
-                console.log('New or updated content is available.');
-              } else {
-                // At this point, everything has been precached.
-                // It's the perfect time to display a "Content is cached for offline use." message.
-                console.log('Content is now available offline!');
-              }
-              break;
-
-            case 'redundant':
-              console.error('The installing service worker became redundant.');
-              break;
-          }
-        };
-      };
-    }).catch(function(e) {
-      console.error('Error during service worker registration:', e);
-    });
+    // navigator.serviceWorker.register('./service-worker.js').then((reg) => {
+    //   console.log(reg, 'reg');
+    //   // updatefound is fired if service-worker.js changes.
+    //   reg.onupdatefound = function () {
+    //     console.log('onupdatefound');
+    //     // The updatefound event implies that reg.installing is set; see
+    //     // https://w3c.github.io/ServiceWorker/#service-worker-registration-updatefound-event
+    //     const installingWorker = reg.installing;
+    //
+    //     installingWorker.onstatechange = function () {
+    //       console.log('onstatechange');
+    //       switch (installingWorker.state) {
+    //         case 'installed':
+    //           if (navigator.serviceWorker.controller) {
+    //             // At this point, the old content will have been purged and the fresh content will
+    //             // have been added to the cache.
+    //             // It's the perfect time to display a "New content is available; please refresh."
+    //             // message in the page's interface.
+    //             console.log('New or updated content is available.');
+    //           } else {
+    //             // At this point, everything has been precached.
+    //             // It's the perfect time to display a "Content is cached for offline use." message.
+    //             console.log('Content is now available offline!');
+    //           }
+    //           break;
+    //
+    //         case 'redundant':
+    //           console.error('The installing service worker became redundant.');
+    //           break;
+    //       }
+    //     };
+    //   };
+    // }).catch(function(e) {
+    //   console.error('Error during service worker registration:', e);
+    // });
 
 
     navigator.serviceWorker.onmessage = function (evt) {
