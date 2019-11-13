44d2724d067ec5d585e488f5e2312d901f79d016
Matthew Flavin <mflavin@hawk.iit.edu>
2019-11-13 14:59:22 -0600

    Pushing change to test service worker.

--- service/service-worker.js
+++ service/service-worker.js
@@ -37,6 +37,8 @@ self.addEventListener('message', function(event) {
 // A more complete example of this given in the immediate-claim recipe.
 self.addEventListener('activate', function(event) {
   console.log('worker active :D');
+  localStorage.removeItem('currentETag');
+  console.log('remove key...');
   event.waitUntil(self.clients.claim());
 
   event.waitUntil(
