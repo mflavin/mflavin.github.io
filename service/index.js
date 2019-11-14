b0a9b5a8cb75938b507b5c6ac3739ec9c79ca5af
Matthew Flavin <mflavin@hawk.iit.edu>
2019-11-14 10:43:23 -0600

    Pushing change to test service worker.

--- service/index.js
+++ service/index.js
@@ -18,7 +18,43 @@ document.addEventListener("DOMContentLoaded", function(){
     // Give an indicator that service workers are supported.
     status.textContent = 'supported';
 
-    navigator.serviceWorker.register('./service-worker.js');
+    // navigator.serviceWorker.register('./service-worker.js');
+
+    navigator.serviceWorker.register('service-worker.js').then(function(reg) {
+      console.log('reg');
+      // updatefound is fired if service-worker.js changes.
+      reg.onupdatefound = function() {
+        console.log('update');
+        // The updatefound event implies that reg.installing is set; see
+        // https://w3c.github.io/ServiceWorker/#service-worker-registration-updatefound-event
+        var installingWorker = reg.installing;
+
+        installingWorker.onstatechange = function() {
+          console.log('state');
+          switch (installingWorker.state) {
+            case 'installed':
+              if (navigator.serviceWorker.controller) {
+                // At this point, the old content will have been purged and the fresh content will
+                // have been added to the cache.
+                // It's the perfect time to display a "New content is available; please refresh."
+                // message in the page's interface.
+                console.log('New or updated content is available.');
+              } else {
+                // At this point, everything has been precached.
+                // It's the perfect time to display a "Content is cached for offline use." message.
+                console.log('Content is now available offline!');
+              }
+              break;
+
+            case 'redundant':
+              console.error('The installing service worker became redundant.');
+              break;
+          }
+        };
+      };
+    }).catch(function(e) {
+      console.error('Error during service worker registration:', e);
+    });
 
     navigator.serviceWorker.onmessage = function (evt) {
       document.getElementById('alert').classList.remove('show');
@@ -58,34 +94,34 @@ document.addEventListener("DOMContentLoaded", function(){
     };
 
     // Listen for any messages from the service worker.
-    // navigator.serviceWorker.addEventListener('message', function(event) {
-    //   // console.log('index.js message gotten');
-    //   // A message has been received, now show the message on the page.
-    //   var clientId = event.data.client;
-    //   var node;
-    //   // A message from this client hasn't been received before, so we need to
-    //   // setup a place to show its messages.
-    //   if (!inbox[clientId]) {
-    //     node = document.createElement('div');
-    //     received.appendChild(node);
-    //     inbox[clientId] = node;
-    //   }
-    //   // Show the message.
-    //   node = inbox[clientId];
-    //   node.textContent = 'Client ' + clientId + ' says: ' + event.data.message;
-    // });
+    navigator.serviceWorker.addEventListener('message', function(event) {
+      // console.log('index.js message gotten');
+      // A message has been received, now show the message on the page.
+      var clientId = event.data.client;
+      var node;
+      // A message from this client hasn't been received before, so we need to
+      // setup a place to show its messages.
+      if (!inbox[clientId]) {
+        node = document.createElement('div');
+        received.appendChild(node);
+        inbox[clientId] = node;
+      }
+      // Show the message.
+      node = inbox[clientId];
+      node.textContent = 'Client ' + clientId + ' says: ' + event.data.message;
+    });
 
-    // message.addEventListener('input', function() {
-    //   console.log('index.js input gotten');
-    //   // There isn't always a service worker to send a message to. This can happen
-    //   // when the page is force reloaded.
-    //   if (!navigator.serviceWorker.controller) {
-    //     status.textContent = 'error: no controller';
-    //     return;
-    //   }
-    //   // Send the message to the service worker.
-    //   navigator.serviceWorker.controller.postMessage(message.value);
-    // });
+    message.addEventListener('input', function() {
+      console.log('index.js input gotten');
+      // There isn't always a service worker to send a message to. This can happen
+      // when the page is force reloaded.
+      if (!navigator.serviceWorker.controller) {
+        status.textContent = 'error: no controller';
+        return;
+      }
+      // Send the message to the service worker.
+      navigator.serviceWorker.controller.postMessage(message.value);
+    });
 
   }
 });
