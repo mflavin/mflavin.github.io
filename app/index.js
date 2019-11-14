1d89526c8482efdce5085e0d29509a6dde02ce5b
Matthew Flavin <mflavin@hawk.iit.edu>
2019-11-14 11:33:53 -0600

    Pushing change to test service worker.

--- app/index.js
+++ app/index.js
@@ -1,47 +1,11 @@
-let newWorker;
-
-function showUpdateBar() {
-  let snackbar = document.getElementById('snackbar');
-  snackbar.className = 'show';
-}
-
-// The click event on the pop up notification
-document.getElementById('reload').addEventListener('click', function(){
-  newWorker.postMessage({ action: 'skipWaiting' });
-});
-
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./service-worker.js');
-  // navigator.serviceWorker.register('./service-worker.js').then(reg => {
-  //   console.log('reg');
-  //   reg.addEventListener('updatefound', () => {
-  //     // A wild service worker has appeared in reg.installing!
-  //     console.log('updatefound');
-  //     newWorker = reg.installing;
-  //
-  //     newWorker.addEventListener('statechange', () => {
-  //       console.log('statechange');
-  //       // Has network.state changed?
-  //       switch (newWorker.state) {
-  //         case 'installed':
-  //           if (navigator.serviceWorker.controller) {
-  //             // new update available
-  //             console.log('new update available');
-  //             showUpdateBar();
-  //           }
-  //           // No update available
-  //           console.log('No update available');
-  //           break;
-  //       }
-  //     });
-  //   });
-  // });
-
-
-  let refreshing;
-  navigator.serviceWorker.addEventListener('controllerchange', function () {
-    if (refreshing) return;
-    window.location.reload();
-    refreshing = true;
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
   });
 }
--- app/service-worker.js
+++ app/service-worker.js
@@ -1,45 +1,68 @@
-var cacheName = 'secondVersion';
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  'dog.jpg',
+  'index.js'
+];
 
-self.addEventListener('activate', function(event) {
-  console.log('worker active :D');
-});
-
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
+  // Perform install steps
   event.waitUntil(
-    caches.open(cacheName)
-      .then(cache => cache.addAll([
-        './index.html'
-      ]))
-  );
-});
-
-self.addEventListener('message', function (event) {
-  if (event.data.action === 'skipWaiting') {
-    self.skipWaiting();
-  }
-});
-
-self.addEventListener('fetch', function (event) {
-  console.log('fetch event, ',event);
-  event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        if (response) {
-          console.log("response.headers.get('ETag')");
-          console.log(response.headers.get('ETag'));
-          return response;
-        }
-        return fetch(event.request);
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
       })
   );
-  evt.waitUntil(
-    update(evt.request)
-    // Finally, send a message to the client to inform it about the
-    // resource is up to date.
-    .then(refresh)
-    // .then((e) => {
-    //   console.log('e, ,', e);
-    //   refresh(e);
-    // })
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+
+  var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
