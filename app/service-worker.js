302fec8b8d40786676ac416ed015cc0f1ae57466
Matthew Flavin <mflavin@hawk.iit.edu>
2019-11-14 13:48:09 -0600

    Pushing change to test service worker.

--- app/service-worker.js
+++ app/service-worker.js
@@ -47,7 +47,7 @@ self.addEventListener('fetch', function (event) {
             caches.match(event.request).then(function (cacheresponse) {
               console.log('event.request, ',event.request);
               console.log('cacheresponse, ',cacheresponse );
-              console.log((cacheresponse.headers.get("ETag" || ''));
+              console.log(cacheresponse.headers.get("ETag" || ''));
                 if ((cacheresponse.headers.get("ETag" || '')) !== response.headers.get("ETag")) {
                   console.log('[ServiceWorker]' + response.url + ' - Cache' + cacheresponse.headers.get("ETag") + "- real" + response.headers.get("ETag"));
                   cache.put(event.request, response.clone()).then(function () {
