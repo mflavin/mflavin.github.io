a3ed64770cca6b1d3f8aefbaee3747277dc7fc09
Matthew Flavin <mflavin@hawk.iit.edu>
2019-11-15 10:19:41 -0600

    Pushing change to test service worker.

--- app/index.js
+++ app/index.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
+  var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
+  var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
+  var CACHE_NAME = APP_PREFIX + VERSION
   document.body.style.background = "darkblue";
   document.getElementById('reload').addEventListener('click', function() {
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function (key, i) {
+        console.log('deleting cache : ' + keyList[i] )
+        return caches.delete(keyList[i])
+      }))
+    })
     location.reload(true);
   });
 
