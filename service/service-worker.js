66a1c9fbbb90b07e3df4cd1192b85faf6fb3109e
Matthew Flavin <mflavin@hawk.iit.edu>
2019-11-15 08:43:19 -0600

    Pushing change to test service worker.

--- service/service-worker.js
+++ service/service-worker.js
@@ -60,7 +60,7 @@ self.addEventListener('install', function(evt) {
   evt.waitUntil(
     caches.open(cacheName)
       .then(cache => cache.addAll([
-        '/',
+        '/service/',
       ]))
   );
 });
