35b182078ddf6d9a6a314a4d7be3960163718b6b
Matthew Flavin <mflavin@hawk.iit.edu>
2019-11-15 07:12:55 -0600

    Pushing change to test service worker.

--- service/index.js
+++ service/index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
+  document.body.style.background = "purple";
   document.getElementById('alertA').addEventListener('click', function() {
     // console.log('reload');
     // navigator.serviceWorker.controller.postMessage({ action: 'clearCache' });
