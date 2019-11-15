454ac7bce1e48d1b6402b7bab88a5ebfbc2c10e4
Matthew Flavin <mflavin@hawk.iit.edu>
2019-11-15 09:58:39 -0600

    Pushing change to test service worker.

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-  document.body.style.background = "lightgreen";
+  document.body.style.background = "lightblue";
   document.getElementById('reload').addEventListener('click', function() {
     window.location.reload();
   });
--- app/service-worker.js
+++ app/service-worker.js
@@ -32,19 +32,9 @@ self.addEventListener('install', function (e) {
 self.addEventListener('activate', function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
-      // `keyList` contains all cache names under your username.github.io
-      // filter out ones that has this app prefix to create white list
-      var cacheWhitelist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX)
-      })
-      // add current cache name to white list
-      cacheWhitelist.push(CACHE_NAME)
-
       return Promise.all(keyList.map(function (key, i) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i] )
-          return caches.delete(keyList[i])
-        }
+        console.log('deleting cache : ' + keyList[i] )
+        return caches.delete(keyList[i])
       }))
     })
   )
