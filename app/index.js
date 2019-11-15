9963d5a8b1704887276d6ee5c660aad0dde605dd
Matthew Flavin <mflavin@hawk.iit.edu>
2019-11-15 10:07:30 -0600

    Pushing change to test service worker.

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-  document.body.style.background = "red";
+  document.body.style.background = "grey";
   document.getElementById('reload').addEventListener('click', function() {
     window.location.reload();
   });
