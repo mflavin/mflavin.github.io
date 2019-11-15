e0db6dad752f01c268850ff3ef7ad3fd560b8ebb
Matthew Flavin <mflavin@hawk.iit.edu>
2019-11-15 07:41:06 -0600

    Pushing change to test service worker.

--- app/service-worker.js
+++ app/service-worker.js
@@ -7,15 +7,15 @@ var cacheFiles = [
 
 
 
-self.addEventListener('install', function (e) {
+self.addEventListener('install', event => {
     console.log("[serviceWorker] installed")
 
-    e.waitUntil(
-        caches.open('cache-update-and-refresh').then(function (cache) {
-            return cache.addAll(cacheFiles)
-        }).then(function () {
-            return self.skipWaiting();
-        }));
+    event.waitUntil(
+      caches.open(cacheName)
+        .then(cache => {
+          return cache.addAll(cacheFiles)
+        });
+    );
     console.log("[serviceWorker] Cached")
 })
 
@@ -24,69 +24,20 @@ self.addEventListener('activate', (event) => {
     event.waitUntil(
         caches.keys().then((cacheNames) => {
             return Promise.all(
-                cacheNames.map((cache) => {
-                    // if (cache !== cacheName) {     //cacheName = 'cache-v1'
-                        return caches.delete(cache); //Deleting the cache
-                    // }
+                cacheNames.filter((cache) => {
+                }).map(function(cache) {
+                  return caches.delete(cache); //Deleting the cache
                 })
-            );
+            });
         })
     );
 });
 
 self.addEventListener('fetch', function (event) {
-    console.log('Navigation Changes?');
-    event.respondWith(
-      caches.match(event.request).then(function (response) {
-        return response || fetch(event.request);
-      })
-    );
-    event.waitUntil(
-      update(event.request)
-        .then(refresh)
-        //   function (response) {
-        //   caches.open(cacheName).then(function (cache) {
-        //     caches.match(event.request).then(function (cacheresponse) {
-        //         console.log('event.request, ',event.request);
-        //         console.log('cacheresponse, ',cacheresponse );
-        //         console.log(cacheresponse.headers.get("ETag" || ''));
-        //         if ((cacheresponse.headers.get("ETag" || '')) !== response.headers.get("ETag")) {
-        //           console.log('[ServiceWorker]' + response.url + ' - Cache' + cacheresponse.headers.get("ETag") + "- real" + response.headers.get("ETag"));
-        //           cache.put(event.request, response.clone()).then(function () {
-        //               refresh(event.request, response);
-        //           });
-        //         }
-        //     });
-        //   });
-        // }
-      // )
-    )
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      console.log('caught fetch..');
+      return caches.match(event.request);
+    })
+  );
 });
-
-function fromCache(request) {
-    return caches.open(cacheName).then(function (cache) {
-        return cache.match(request);
-    });
-}
-
-function update(request) {
-    return caches.open(cacheName).then(function (cache) {
-        return fetch(request).then(function (response) {
-            return response;
-        });
-    });
-}
-
-
-function refresh(response) {
-    return self.clients.matchAll().then(function (clients) {
-        clients.forEach(function (client) {
-            var message = {
-                type: 'refresh',
-                url: response,
-                eTag: response.headers.get('ETag')
-            };
-            client.postMessage(JSON.stringify(message));
-        });
-    });
-}
