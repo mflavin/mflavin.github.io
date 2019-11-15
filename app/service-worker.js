ce821f615de81a08b4e104cfc99a619b9f66178e
Matthew Flavin <mflavin@hawk.iit.edu>
2019-11-15 08:20:24 -0600

    Pushing change to test service worker.

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,32 +1,11 @@
-var cacheName = 'cache-update-and-refresh';
-var cacheFiles = [
-    './',
-    // list all the assests you want to list
+var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
+var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
+var CACHE_NAME = APP_PREFIX + VERSION
+var URLS = [                            // Add URL you want to cache in this list.
+  '/app/',                     // If you have separate JS/CSS files,
+  '/app/index.html'            // add path to those files here
 ]
 
-
-
-// On install, cache some resource.
-self.addEventListener('install', function(evt) {
-  evt.waitUntil(
-    caches.open(cacheName)
-      .then(cache => cache.addAll(cacheFiles))
-  );
-});
-
-self.addEventListener('activate', (event) => {
-    console.info('Event: Activate');
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.filter((cache) => {
-                }).map(function(cache) {
-                  return caches.delete(cache); //Deleting the cache
-                })
-            );
-        })
-    );
-});
 
 self.addEventListener('fetch', function (event) {
   event.respondWith(
@@ -36,3 +15,36 @@ self.addEventListener('fetch', function (event) {
     })
   );
 });
+
+
+// Cache resources
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(URLS)
+    })
+  )
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create white list
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      // add current cache name to white list
+      cacheWhitelist.push(CACHE_NAME)
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] )
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
