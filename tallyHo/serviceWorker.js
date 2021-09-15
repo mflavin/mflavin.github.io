8a5c9d0b564bde952bb0d388ad0151ce711afab5
mflavin <mflavin@hawk.iit.edu>
2021-09-14 22:05:56 -0500

    Add cacheFiles

--- tallyHo/serviceWorker.js
+++ tallyHo/serviceWorker.js
@@ -1,6 +1,15 @@
 /* eslint-disable */
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.4/workbox-sw.js');
+const cacheFiles = [
+	{
+		revision: '090420212205',
+    url: '/test/',
+	}
+];
+
+self.__precacheManifest = cacheFiles.concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
 const handler = workbox.precaching.createHandlerBoundToURL('/tallyHo/');
 const navigationRoute = new workbox.routing.NavigationRoute(handler);
 workbox.routing.registerRoute(navigationRoute);
