84f46ca705c140259d341b80a1ea20a12309da67
mflavin <mflavin@hawk.iit.edu>
2021-09-15 12:05:00 -0500

    Add fetch to sw

--- tallyHo/serviceWorker.js
+++ tallyHo/serviceWorker.js
@@ -33,3 +33,11 @@ workbox.routing.registerRoute(
     cacheName: 'static-resources',
   })
 );
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+      caches.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+      })
+  );
+});
