76d927973bf8117ce6aae90a145ff0178277f1e6
mflavin <mflavin@hawk.iit.edu>
2021-09-15 12:08:43 -0500

    Remove fetch add purpose to icon in manifest

--- tallyHo/serviceWorker.js
+++ tallyHo/serviceWorker.js
@@ -33,11 +33,3 @@ workbox.routing.registerRoute(
     cacheName: 'static-resources',
   })
 );
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-      caches.match(event.request).then(function(response) {
-          return response || fetch(event.request);
-      })
-  );
-});
