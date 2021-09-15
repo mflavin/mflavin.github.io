941c3ee795ceb0a8427d1af929ec8f33b67776cf
mflavin <mflavin@hawk.iit.edu>
2021-09-14 21:57:53 -0500

    Remove undefined variable

--- tallyHo/serviceWorker.js
+++ tallyHo/serviceWorker.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.4/workbox-sw.js');
-self.__precacheManifest = cacheFiles.concat(self.__precacheManifest || []);
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 const handler = workbox.precaching.createHandlerBoundToURL('/');
 const navigationRoute = new workbox.routing.NavigationRoute(handler);
