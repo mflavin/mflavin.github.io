6a1208cc90a1a784f8c6b78417d8b9ca734726a9
mflavin <mflavin@hawk.iit.edu>
2021-09-14 21:55:43 -0500

    Update menu styling for mobile. Add serviceworker

---
+++ tallyHo/serviceWorker.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.4/workbox-sw.js');
+self.__precacheManifest = cacheFiles.concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+const handler = workbox.precaching.createHandlerBoundToURL('/');
+const navigationRoute = new workbox.routing.NavigationRoute(handler);
+workbox.routing.registerRoute(navigationRoute);
