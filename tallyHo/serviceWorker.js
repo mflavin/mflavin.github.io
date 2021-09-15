dff21dcd153cc935801ddb230abfe56365460d96
mflavin <mflavin@hawk.iit.edu>
2021-09-14 22:00:50 -0500

    Add scope to sw

--- tallyHo/serviceWorker.js
+++ tallyHo/serviceWorker.js
@@ -1,6 +1,5 @@
 /* eslint-disable */
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.4/workbox-sw.js');
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 const handler = workbox.precaching.createHandlerBoundToURL('/');
 const navigationRoute = new workbox.routing.NavigationRoute(handler);
