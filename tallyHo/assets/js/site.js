7ec3b354e4455ad1725ab7b26c6a7a0c9c72e9dc
mflavin <mflavin@hawk.iit.edu>
2021-09-19 13:31:25 -0500

    Fixed async call. Updated menu data

--- tallyHo/assets/js/site.js
+++ tallyHo/assets/js/site.js
@@ -5,19 +5,19 @@
 			.then(() => console.log('Service Worker Registered'))
 			.catch((e) => console.log('idk error ', e));
 
-	const getMenuData = (section) => {
+	const getMenuData = await (section) => {
 		const menuData = await fetch(`./assets/js/menu/${section}.json`);
 		const data = await menuData.json();
 		document.getElementById(section).innerHTML = tmpl('item-card-tmpl', data);
 	};
 		// Template data for Appetizers
-	getMenuData('appetizers');
+	await getMenuData('appetizers');
 
 	// Template data for Favorites
-	getMenuData('favorites');
+	await getMenuData('favorites');
 
 	// Template data for Soup, Salad, & Sandwiches
-	getMenuData('soups');
+	await getMenuData('soups');
 
 	// Set menu tabs for food sections
 	document.getElementById('menu').innerHTML = tmpl('menu-tmpl', ['appetizers', 'favorites', 'soups']);
