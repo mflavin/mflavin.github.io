15109aa495636872261d88b5be85217afe6cc60f
mflavin <mflavin@hawk.iit.edu>
2021-09-19 12:54:16 -0500

    Put menu data into seperate json

--- tallyHo/assets/js/site.js
+++ tallyHo/assets/js/site.js
@@ -1,82 +1,30 @@
 (async () => {
-	try {
-		const d = await fetch('./assets/js/employees.json');
-		console.log('d', d);
-		const data = await d.json()
-		console.log('data', data);
-	} catch (e) {
-		console.log('error , ', e);
-	}
-
 	// Register service worker
 	if ('serviceWorker' in navigator)
 		navigator.serviceWorker.register('serviceWorker.js')
 			.then(() => console.log('Service Worker Registered'))
 			.catch((e) => console.log('idk error ', e));
 
-
+	// Get Menu data
+	const menuData = await fetch('./assets/js/menu.json');
+	const data = await menuData.json();
 	// Template data for Appetizers
-	const appetizersData = {
-	  title: 'Appetizers',
-	  menu: [
-			{
-				name: 'Nachos',
-				description: 'Homemade torilla chips topped with choice of beef or chicken, onion, tamato, and shredded cheese. Served with salsa and sour cream on the side.',
-				price: '14.00'
-			},
-		]
-	};
+	const appetizersData = data.find(m => m.title === 'Appetizers');
 	document.getElementById('Appetizers').innerHTML = tmpl('item-card-tmpl', appetizersData);
 
 	// Template data for Favorites
-	const favoritesData = {
-	  title: 'Favorites',
-	  menu: [
-			{
-				name: 'Nachos',
-				description: 'Homemade torilla chips topped with choice of beef or chicken, onion, tamato, and shredded cheese. Served with salsa and sour cream on the side.',
-				price: '14.00'
-			},
-		]
-	};
+	const favoritesData = data.find(m => m.title === 'Favorites');
 	document.getElementById('Favorites').innerHTML = tmpl('item-card-tmpl', favoritesData);
 
 	// Template data for Salads
-	const saladsData = {
-		title: 'Salads',
-		menu: [
-			{
-				name: 'Nachos',
-				description: 'Homemade torilla chips topped with choice of beef or chicken, onion, tamato, and shredded cheese. Served with salsa and sour cream on the side.',
-				price: '14.00'
-			},
-		]
-	};
+	const saladsData = data.find(m => m.title === 'Salads');
 	document.getElementById('Salads').innerHTML = tmpl('item-card-tmpl', saladsData);
 
 	// Template data for Steaks
-	const steaksData = {
-		title: 'Steaks',
-		menu: [
-			{
-				name: 'Nachos',
-				description: 'Homemade torilla chips topped with choice of beef or chicken, onion, tamato, and shredded cheese. Served with salsa and sour cream on the side.',
-				price: '14.00'
-			},
-		]
-	};
+	const steaksData = data.find(m => m.title === 'Steaks');
 	document.getElementById('Steaks').innerHTML = tmpl('item-card-tmpl', steaksData);
 
 	// Template data for Country
-	const countryData = {
-		title: 'Country',
-		menu: [
-			{
-				name: 'Nachos',
-				description: 'Homemade torilla chips topped with choice of beef or chicken, onion, tamato, and shredded cheese. Served with salsa and sour cream on the side.',
-				price: '14.00'
-			},
-		]
-	};
+	const countryData = data.find(m => m.title === 'Country');
 	document.getElementById('Country').innerHTML = tmpl('item-card-tmpl', countryData);
 })();
