1c1b0d2a715e2463374691c89771eb5623ede004
mflavin <mflavin@hawk.iit.edu>
2022-06-20 22:23:01 -0500

    Cache navigations

--- sw.js
+++ sw.js
@@ -2,30 +2,29 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js',
 );
 
-// Note: Ignore the error that Glitch raises about workbox being undefined.
 workbox.setConfig({
   debug: true,
 });
 
-workbox.precaching.precacheAndRoute([
-  'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css',
-]);
+// Enable navigation preload.
+workbox.navigationPreload.enable();
 
-// Demonstrates using default cache
-workbox.routing.registerRoute(
-  new RegExp('.*\\.(?:html)'),
-  new workbox.strategies.NetworkFirst(),
-);
+// Swap in NetworkOnly, CacheFirst, or StaleWhileRevalidate as needed.
+const strategy = new workbox.strategies.NetworkFirst({
+  cacheName: 'cached-navigations',
+  plugins: [
+    // Any plugins, like workbox.expiration, etc.
+  ],
+});
 
-// Demonstrates a custom cache name for a route.
-workbox.routing.registerRoute(
-  new RegExp('.*\\.(?:png|jpg|jpeg|svg|gif)'),
-  new workbox.strategies.CacheFirst({
-    cacheName: 'image-cache',
-    plugins: [
-      new workbox.expiration.ExpirationPlugin({
-        maxEntries: 3,
-      }),
-    ],
-  }),
-);
+const navigationRoute = new workbox.routing.NavigationRoute(strategy, {
+  // Optionally, provide a allowlist/denylist of RegExps to determine
+  // which paths will match this route.
+  // allowlist: [],
+  // denylist: [],
+});
+
+workbox.routing.registerRoute(navigationRoute);
+
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
