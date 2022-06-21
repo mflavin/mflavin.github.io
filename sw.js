6c390c7a8dd01df2660581d1a28f7542b9c384c2
mflavin <mflavin@hawk.iit.edu>
2022-06-20 22:40:28 -0500

    Fix async func

--- sw.js
+++ sw.js
@@ -6,10 +6,10 @@ const CACHE_NAME = "V1"
  * Generally, we cache static resources that allow the website to run offline
  */
 this.addEventListener('install', async function() {
-    const cache = await caches.open(CACHE_NAME);
-    cache.addAll([
-        '/index.html',
-    ])
+  const cache = await caches.open(CACHE_NAME);
+  cache.addAll([
+    '/index.html',
+  ])
 })
 
 /**
@@ -20,39 +20,39 @@ this.addEventListener('install', async function() {
  * Here, we are going to use cache first strategy
  */
 self.addEventListener('fetch', event => {
-    //We defind the promise (the async code block) that return either the cached response or the network one
-    //It should return a response object
-    const getCustomResponsePromise = async => {
-        console.log(`URL ${event.request.url}`, `location origin ${location}`)
+  //We defind the promise (the async code block) that return either the cached response or the network one
+  //It should return a response object
+  const getCustomResponsePromise = async () => {
+    console.log(`URL ${event.request.url}`, `location origin ${location}`)
 
-        try {
-            //Try to get the cached response
-            const cachedResponse = await caches.match(event.request)
-            if (cachedResponse) {
-                //Return the cached response if present
-                console.log(`Cached response ${cachedResponse}`)
-                return cachedResponse
-            }
+    try {
+      //Try to get the cached response
+      const cachedResponse = await caches.match(event.request)
+      if (cachedResponse) {
+          //Return the cached response if present
+          console.log(`Cached response ${cachedResponse}`)
+          return cachedResponse
+      }
 
-            //Get the network response if no cached response is present
-            const netResponse = await fetch(event.request)
-            console.log(`adding net response to cache`)
+      //Get the network response if no cached response is present
+      const netResponse = await fetch(event.request)
+      console.log(`adding net response to cache`)
 
-            //Here, we add the network response to the cache
-            let cache = await caches.open(CACHE_NAME)
+      //Here, we add the network response to the cache
+      let cache = await caches.open(CACHE_NAME)
 
-            //We must provide a clone of the response here
-            cache.put(event.request, netResponse.clone())
+      //We must provide a clone of the response here
+      cache.put(event.request, netResponse.clone())
 
-            //return the network response
-            return netResponse
-        } catch (err) {
-            console.error(`Error ${err}`)
-            throw err
-        }
+      //return the network response
+      return netResponse
+    } catch (err) {
+      console.error(`Error ${err}`)
+      throw err
     }
+  }
 
-    //In order to override the default fetch behavior, we must provide the result of our custom behavoir to the
-    //event.respondWith method
-    event.respondWith(getCustomResponsePromise())
+  //In order to override the default fetch behavior, we must provide the result of our custom behavoir to the
+  //event.respondWith method
+  event.respondWith(getCustomResponsePromise())
 })
