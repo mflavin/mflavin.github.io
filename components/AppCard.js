bdd99f1ceaf752382730851e05186e2b990163d8
mflavin <mflavin@hawk.iit.edu>
2023-11-13 11:38:55 -0600

    Update AppCard component

--- components/AppCard.js
+++ components/AppCard.js
@@ -1,87 +1,111 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const styles = `
+    .svg {
+      width: 100%;
+      width: 10rem;
+    }
+
+    .icon {
+      width: 100%;
+      height: 12rem;
+      min-height: 12rem;
+      border-radius: 1rem;
+      border-bottom-left-radius: 0.25rem;
+      border-bottom-right-radius: 0.25rem;
+      background: rgb(14, 165, 233);
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    a:hover {
+      color: #000000;
+    }
+
+    .container {
+      height: 100%;
+    }
+
+    .col-body {
+      padding: 2rem;
+    }
+
+    .col-header {
+      margin-bottom: 0.25rem;
+      font-size: 0.875rem;
+      line-height: 1.25rem;
+      font-weight: 600;
+      letter-spacing: 0.025em;
+      text-transform: uppercase;
+      color: rgb(56, 189, 248);
+    }
+
+    .col-link {
+      font-size: 1.125rem;
+      line-height: 1.75rem;
+      font-weight: 500;
+      color: #000000;
+    }
+
+    .col-text {
+      margin-top: 0.5rem;
+      color: rgb(100, 116, 139);
+    }
+
+    @media (min-width: 768px) {
+      .svg {
+        width: 8rem;
+        padding: 1rem;
+      }
+
+      .container {
+        display: flex;
+      }
+
+      .icon {
+        flex-shrink: 0;
+        height: 100%;
+        width: 12rem;
+        border-radius: 1rem;
+        border-bottom-right-radius: 0.25rem;
+        border-top-right-radius: 0.25rem;
+      }
+    }
+  `;
+
   class AppCard extends HTMLElement {
     constructor() {
       super();
 
-      const shadowRoot = this.attachShadow({ mode: 'open' });
+      // Create shadow root
+      this.attachShadow({ mode: 'open' });
 
       // Add the component's styles to the shadow root
       const style = document.createElement('style');
-      style.textContent = `
-        .svg {
-          width: 100%;
-          width: 10rem;
-        }
+      style.textContent = styles;
+      this.shadowRoot.appendChild(style);
 
-        .icon {
-          width: 100%;
-          height: 12rem;
-          min-height: 12rem;
-          border-radius: 1rem;
-          border-bottom-left-radius: 0.25rem;
-          border-bottom-right-radius: 0.25rem;
-          background: rgb(14, 165, 233);
-          display: flex;
-          align-items: center;
-          justify-content: center;
-        }
+      // Build and add container to shadow root
+      this.container = document.createElement('div');
+      this.container.classList.add('container');
+      this.shadowRoot.appendChild(this.container);
+    }
 
-        a:hover {
-          color: #000000;
-        }
+    connectedCallback() {
+      this.render();
+    }
 
-        .container {
-          height: 100%;
-        }
+    static get observedAttributes() {
+      return ['text', 'desc', 'href', 'svg'];
+    }
 
-        .col-body {
-          padding: 2rem;
-        }
+    attributeChangedCallback(name, oldValue, newValue) {
+      if (oldValue !== newValue) {
+        this.render();
+      }
+    }
 
-        .col-header {
-          margin-bottom: 0.25rem;
-          font-size: 0.875rem;
-          line-height: 1.25rem;
-          font-weight: 600;
-          letter-spacing: 0.025em;
-          text-transform: uppercase;
-          color: rgb(56, 189, 248);
-        }
-
-        .col-link {
-          font-size: 1.125rem;
-          line-height: 1.75rem;
-          font-weight: 500;
-          color: #000000;
-        }
-
-        .col-text {
-          margin-top: 0.5rem;
-          color: rgb(100, 116, 139);
-        }
-
-        @media (min-width: 768px) {
-          .svg {
-            width: 8rem;
-            padding: 1rem;
-          }
-
-          .container {
-            display: flex;
-          }
-
-          .icon {
-            flex-shrink: 0;
-            height: 100%;
-            width: 12rem;
-            border-radius: 1rem;
-            border-bottom-right-radius: 0.25rem;
-            border-top-right-radius: 0.25rem;
-          }
-        }
-      `;
-      shadowRoot.appendChild(style);
-
+    render() {
       const wrapper = document.createElement('div');
       wrapper.classList.add('container');
       // Get the value of the 'text' attribute
@@ -93,26 +117,17 @@ document.addEventListener('DOMContentLoaded', () => {
       // Get the value of the 'svg' attribute
       const svg = this.getAttribute('svg');
       // Set the inner HTML of the component
-      wrapper.innerHTML = `
-      <a href="${href}" style="display: contents">
-        <div class="icon">
-          <img class="svg" src="./${svg}.svg" alt="My Happy SVG" />
-        </div>
-        <div class="col-body">
-          <div class="col-header">${text}</div>
-          <div class="col-text">${desc}</div>
-        </div>
-        </a>
-      `;
-      shadowRoot.appendChild(wrapper);
-    }
-
-    connectedCallback() {
-      this.classList.add('col');
-    }
-
-    disconnectedCallback() {
-      this.classList.remove('col');
+      this.container.innerHTML = `
+        <a href="${href}" style="display: contents">
+          <div class="icon">
+            <img class="svg" src="./${svg}.svg" alt="My Happy SVG" />
+          </div>
+          <div class="col-body">
+            <div class="col-header">${text}</div>
+            <div class="col-text">${desc}</div>
+          </div>
+          </a>
+        `;
     }
   }
   customElements.define('app-card', AppCard);
