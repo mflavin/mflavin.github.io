bd53cbc4d47ac090de0e064dd13ec2e06dcb8238
mflavin <mflavin@hawk.iit.edu>
2024-03-08 14:47:54 -0600

    Run prettier-plugin-tailwindcss

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # I am mflavin
+
 - Passionate and creative full-stack software engineer from Illinois
 - Full-stack developer with 5 years of experience specializing in building user-friendly interfaces and APIs
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,30 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'selector',
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: "selector",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       screens: {
-        xs: '420px',
+        xs: "420px",
       },
       colors: {
-        light: '#FBFCFE',
-        dark: '#0F172A',
+        light: "#FBFCFE",
+        dark: "#0F172A",
         toolbar: {
-          DEFAULT: '#3867D60D',
-          dark: '#88a4e60e',
+          DEFAULT: "#3867D60D",
+          dark: "#88a4e60e",
         },
         primary: {
-          DEFAULT: '#0C121E',
-          dark: '#FBFCFE',
+          DEFAULT: "#0C121E",
+          dark: "#FBFCFE",
         },
         secondary: {
-          DEFAULT: '#0C121ED9',
-          dark: '#FBFCFED9',
+          DEFAULT: "#0C121ED9",
+          dark: "#FBFCFED9",
         },
         accent: {
-          DEFAULT: '#006877',
-          dark: '#52d7f0',
+          DEFAULT: "#006877",
+          dark: "#52d7f0",
         },
       },
     },
