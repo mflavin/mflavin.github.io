de856e6a2434961fdba84c01556f4f8a6a66815e
mflavin <mflavin@hawk.iit.edu>
2025-05-31 13:59:39 -0500

    Move education and work data to data files.

---
+++ src/data/education.ts
@@ -0,0 +1,11 @@
+import illinoisTechLogo from '@/images/illinois_tech_logo.jpg';
+
+export const education = [
+  {
+    logo: illinoisTechLogo,
+    degree: "Bachelor's degree, Software Development",
+    institution: 'Illinois Institute of Technology',
+    startYear: 2015,
+    endYear: 2019,
+  },
+];
